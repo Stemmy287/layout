4bc3da563bee323ce28db332882dfc536a459c12
Stemmy287 <steem287@gmail.com>
2022-12-08 17:22:31 +0300

    Updates

---
+++ precache-manifest.65e27d959f11f142f864d8cd1885115a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba77b8dba58d8d689af7",
+    "url": "/layout/static/css/main.196ebdbe.chunk.css"
+  },
+  {
+    "revision": "ba77b8dba58d8d689af7",
+    "url": "/layout/static/js/main.ba77b8db.chunk.js"
+  },
+  {
+    "revision": "0a3fad24fe19322116ed",
+    "url": "/layout/static/js/1.0a3fad24.chunk.js"
+  },
+  {
+    "revision": "6c84bbda6ae229135a23",
+    "url": "/layout/static/js/2.6c84bbda.chunk.js"
+  },
+  {
+    "revision": "4dbf36d8d2f40aa10c5d",
+    "url": "/layout/static/js/runtime~main.4dbf36d8.js"
+  },
+  {
+    "revision": "97bc7665a10e575ccc91587444e0808d",
+    "url": "/layout/index.html"
+  }
+];
